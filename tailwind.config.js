901e05f670a5e2a4592ec585201f1c2912f2a420
aaairam <magildaceria@gmail.com>
2025-06-30 09:08:10 +0800

    Created new tasks

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#192f57",
+        secondary: "466885",
+      },
+      fontFamily: {
+        poppins: "Poppins",
+      }
+    },
+  },
+  plugins: [],
+};
