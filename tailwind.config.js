2788adbea6090976f77f1284bcdac57e42f87deb
aaairam <magildaceria@gmail.com>
2025-07-03 15:58:42 +0800

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         primary: "#192f57",
         secondary: "#466885",
       },
       fontFamily: {
         poppins: "Poppins",
-      }
+      },
     },
   },
   plugins: [],
